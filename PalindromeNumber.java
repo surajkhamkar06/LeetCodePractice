30a07d181e2f54693cd7a39873365ca960888745
Khamkar Suraj <135463570+surajkhamkar06@users.noreply.github.com>
2025-09-01 13:08:00 +0530

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,5 +1,23 @@
 package package1;
 
-public class PalindromeNumber {
+public class PalindromeNumber
+{
+   int temp = x;
+        int rev = 0;
 
+        while(x>0)
+        {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x = x/10;
+        }
+        if(temp==rev)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
 }
+
