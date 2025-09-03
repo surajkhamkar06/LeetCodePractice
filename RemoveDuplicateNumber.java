b316461e1d27a1e304e1e757fd0dc6cc8db3cd3c
Khamkar Suraj <135463570+surajkhamkar06@users.noreply.github.com>
2025-09-03 15:35:09 +0530

    Remove Duplicate Number

---
+++ RemoveDuplicateNumber.java
@@ -0,0 +1,24 @@
+package package1;
+
+public class RemoveDuplicateNumber 
+{
+	 public int removeDuplicates(int[] nums) 
+	    {
+	        int count =0;
+	        for(int i=0; i<nums.length; i++)
+	        {
+	            if(i<nums.length-1 && nums[i]==nums[i+1])
+	            {
+	                continue;
+	            }
+	            else
+	            {
+	                nums[count] = nums[i];
+	                count++;
+	            }
+	        }
+	        return count;
+
+	    }
+
+}
